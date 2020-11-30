3197691b37a8eb1503298241a8e92852e655ba62
DaviReisVieira <vieira08davi38@gmail.com>
2020-11-30 15:01:07 -0300

    Changes in gender and Headers

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,22 @@ function setName(){
     var inputText = inputElement.value;
     localStorage.setItem('playerName',inputText)
     inputElement.value = '';
+
+    var val = getRadioValue( document.getElementById('form_inicial'), 'gender' );
+    localStorage.setItem('gender',val)
 }
 
 aElement.onclick = setName;
 
+function getRadioValue(form, name) {
+    var val;
+    var radios = form.elements[name];
+    
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { 
+            val = radios[i].value; 
+            break; 
+        }
+    }
+    return val;
+}
--- assets/js/tela_logada.js
+++ assets/js/tela_logada.js
@@ -1,5 +1,20 @@
 var h1Element = document.querySelector('header h1')
 
+var imgElement = document.querySelector('header img')
+
 var playerName = localStorage.getItem('playerName')
     
 h1Element.innerHTML=playerName;
+
+function selectGenderImage(){
+    var genderSelected=localStorage.getItem('gender')
+    if (genderSelected =='f'){
+        imgElement.src='../assets/img/avatarF.jpg'
+    } else if (genderSelected =='m'){
+        imgElement.src='../assets/img/avatarM.jpg'
+    } else {
+        imgElement.src='../assets/img/logoEsports.png'
+    }
+}
+
+selectGenderImage()
